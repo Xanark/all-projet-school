f97560d83ce6d1e6925c41b6933787581e8f6abc
Xanark <92733155+Xanark@users.noreply.github.com>
2023-07-26 11:52:46 +0200

    Create tp.py

---
+++ cours/tp.py
@@ -0,0 +1,19 @@
+
+
+class Graphe_or:
+
+    def __init__(self,n):
+        self.nombre = n
+        self.adj = [n*[False] for _ in range(n)]
+
+
+    def ajouter_arc(self,  s1, s2):
+        
+
+
+
+
+
+
+graphe =Graphe_or(4)
+print(graphe)
